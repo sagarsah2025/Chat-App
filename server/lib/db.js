c7cac8ed7491f0913037d779fae9cf9252e551b5
sagar <sagarfbg2019@gmail.com>
2025-08-29 15:35:55 +0530

    first commit

---
+++ server/lib/db.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose"
+
+// Function to connect to the mongodb database
+export const connectDB = async () =>{
+    try {
+
+        mongoose.connection.on('connected', ()=> console.log('Database Connected'));
+        await mongoose.connect(`${process.env.MONGODB_URI}/
+        cat-app`)
+    } catch (error) {
+        console.log(error);
+    }
+}
